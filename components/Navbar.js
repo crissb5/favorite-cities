11bf6e40f0365ee52348f5feccfe697474de8fb9
crissb5 <cristinazaharia22@gmail.com>
2024-11-13 13:23:32 +0200

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+      <Link href="/" passHref style={{ margin: "0 10px" }}>
+        Home
+      </Link>
+      <Link href="/search" passHref style={{ margin: "0 10px" }}>
+        Search
+      </Link>
+      <Link href="/city" passHref style={{ margin: "0 10px" }}>
+        City
+      </Link>
+      <Link href="/favorites" passHref style={{ margin: "0 10px" }}>
+        Favorites
+      </Link>
+    </nav>
+  );
+}
